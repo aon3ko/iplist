dd0147e3ac97b8adf3e0fe5bef884d9f017eb524
EXSERENS <7828319+EXSERENS@users.noreply.github.com>
2020-03-16 15:12:47 +0800

    autoadd-ipset.sh: support another ipset type. for now, it's only hash:x.

--- autoadd-ipset.sh
+++ autoadd-ipset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 execpath=/etc/iplist
-execfile=autoadd.conf
+execconf=autoadd.conf
 
 if [ ! -z $1 ]; then
 	echo "usage: autoadd-ipset.sh [config file location]\
@@ -8,10 +8,14 @@ if [ ! -z $1 ]; then
 	not implement yet :)"
 fi
 
-while IFS=' ' read setfile setname; do
+while IFS=' ' read setfile setname type; do
 	if test -f $execpath/inet/$setfile; then
 		echo "adding $setname IPv4 part."
-		ipset create $setname-4 hash:net family inet
+		if [ ! -z "$type" ]; then
+			ipset create $setname-4 hash:$type family inet
+		else
+			ipset create $setname-4 hash:net family inet
+		fi
 		ipset flush $setname-4
 		while read iprange; do
 			ipset add $setname-4 $iprange
@@ -20,11 +24,15 @@ while IFS=' ' read setfile setname; do
 	fi
 	if test -f $execpath/inet6/$setfile; then
 		echo "adding $setname IPv6 part."
-		ipset create $setname-6 hash:net family inet6
+		if [ ! -z "$type" ]; then
+			ipset create $setname-6 hash:$type family inet6
+		else
+			ipset create $setname-6 hash:net family inet6
+		fi
 		ipset flush $setname-6
 		while read iprange; do
 			ipset add $setname-6 $iprange
 		done < $execpath/inet6/$setfile
 		echo "adding $setname IPv6 done."
 	fi
-done < $execpath/$execfile
+done < $execpath/$execconf
