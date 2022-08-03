46f3cd14731c3522aaec8d2b7a149044a4733321
EXSERENS <7828319+EXSERENS@users.noreply.github.com>
2022-08-04 04:02:30 +0800

    iplist-add.c: simply support nft XD still lots of work need to do

--- iplist-add.c
+++ iplist-add.c
@@ -1,120 +1,196 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <libipset/ipset.h>
+//#include <arguments.h>
 
-#define LENGTH_OF_TYPE 15
-#define LENGTH_OF_NAME 63
-#define LENGTH_OF_PATH 127
-#define LENGTH_OF_LOCATION 255
+#define SELECT_NFT
+#ifdef SELECT_IPSET
+	#include <libipset/ipset.h>
+	#define SELECTED
+#endif
+#ifdef SELECT_NFT
+	#include <nftables/libnftables.h>
+	#define SELECTED
+#endif
+#ifndef SELECTED
+	#include <libipset/ipset.h>
+	#include <nftables/libnftables.h>
+#endif
 
-char workpath[LENGTH_OF_PATH] = "/etc/iplist";
-char conffile[LENGTH_OF_NAME] = "autoadd.conf";
+char workpath[128] = "/etc/iplist";
+char conffile[64] = "autoadd.conf";
 
-// char *argvWrapper(char argv[]);
+enum iplist_flags {
+	V4_CLEARED = (1 << 0),
+	V6_CLEARED = (1 << 1),
+};
+
+union iplist {
+	struct {
+		char name[64];
+		char flags;
+	};
+	struct {
+		unsigned int size;
+		void *operationInterface;
+	};
+};
+
+union iplist *iplist_init(void *operationInterface) {
+	char commandBuf[256];
+	union iplist *iplist = calloc(1, sizeof(union iplist));
+	iplist->size = 1;
+	iplist->operationInterface = operationInterface;
+/*
+#ifdef SELECT_IPSET
+	sprintf(commandBuf,"-n list");
+	ipset_parse_line(iplist->operationInterface, commandBuf);
+#endif
+#ifdef SELECT_NFT
+	sprintf(commandBuf,"list sets");
+	nft_run_cmd_from_buffer(iplist->operationInterface, commandBuf);
+	char nftLineBuf[256];
+	while (fgets(nftLineBuf, 256, nft_ctx_get_output_buffer(iplist->operationInterface))) {
+
+	}
+#endif
+*/
+	return iplist;
+}
+
+int iplist_append(union iplist **dst, FILE *iplistDb, const char *iplistName, const char *p) {
+	char commandBuf[256], readBuf[64], flag = 0;
+	int counter = 0;
+	union iplist *currentLocation = NULL;
+
+	for (int pos = 1; pos < (*dst)->size; pos++)
+		if (strcmp((*dst)->name, iplistName) == 0) {
+			currentLocation = (*dst) + pos;
+			break;
+		}
+	if (currentLocation == NULL) {
+		*dst = realloc(*dst, sizeof(union iplist) * (++(*dst)->size));
+		currentLocation = (*dst) + (*dst)->size - 1;
+		strcpy(currentLocation->name, iplistName);
+#ifdef SELECT_IPSET
+		sprintf(commandBuf,"create %s-4 hash:net family inet", currentLocation->name);
+		ipset_parse_line((*dst)->operationInterface, commandBuf);
+		sprintf(commandBuf,"create %s-6 hash:net family inet6", currentLocation->name);
+		ipset_parse_line((*dst)->operationInterface, commandBuf);
+#endif
+#ifdef SELECT_NFT
+		sprintf(commandBuf, "add set ip %s %s { type ipv4_addr; flags interval; }", p, currentLocation->name);
+		nft_run_cmd_from_buffer((*dst)->operationInterface, commandBuf);
+		sprintf(commandBuf, "add set ip6 %s %s { type ipv6_addr; flags interval; }", p, currentLocation->name);
+		nft_run_cmd_from_buffer((*dst)->operationInterface, commandBuf);
+#endif
+		currentLocation->flags = V4_CLEARED | V6_CLEARED;
+	}
+
+	if (currentLocation->flags != (V4_CLEARED | V6_CLEARED)) {
+#ifdef SELECT_IPSET
+#endif
+#ifdef SELECT_NFT
+#endif
+	}
+	while (fgets(readBuf, 64, iplistDb) != NULL) {
+#ifdef SELECT_IPSET
+		if (strchr(readBuf, '.') != NULL)
+			sprintf(commandBuf, "add %s-4 %s", iplistName, readBuf);
+		if (strchr(readBuf, ':') != NULL)
+			sprintf(commandBuf, "add %s-6 %s", iplistName, readBuf);
+		ipset_parse_line((*dst)->operationInterface, commandBuf); counter++;
+#endif
+#ifdef SELECT_NFT
+		if (strchr(readBuf, '.') != NULL)
+			sprintf(commandBuf, "add element ip %s %s { %s }", p, iplistName, readBuf);
+		if (strchr(readBuf, ':') != NULL)
+			sprintf(commandBuf, "add element ip6 %s %s { %s }", p, iplistName, readBuf);
+		nft_run_cmd_from_buffer((*dst)->operationInterface, commandBuf); counter++;
+#endif
+	}
+
+	fclose(iplistDb);
+	printf(" done with %d record(s).\n", counter);
+	return 0;
+}
+
+void wa_ipset_print() {
+
+}
 
 int main(int argc, char *argv[]) {
+#ifdef SELECT_IPSET
 	ipset_load_types();
-	struct ipset *ipsetInterface = ipset_init();
+	struct ipset *operationInterface = ipset_init();
+	ipset_custom_printf(operationInterface, NULL, NULL, ipsetPrint, NULL);
+#endif
+#ifdef SELECT_NFT
+	struct nft_ctx *operationInterface = nft_ctx_new(NFT_CTX_DEFAULT);
+	//nft_ctx_buffer_output(operationInterface);
+	char nft_table_name[64] = "neko";
+#endif
+	union iplist *iplist = iplist_init(operationInterface);
 
 	for (int pos = 1; pos < argc; pos++)
         	if (argv[pos][0] == '-') switch (argv[pos][1]) {
 		case 'c':
 			break;
+#ifdef SELECT_NFT
+		case 't':
+			strcpy(nft_table_name, argv[pos + 1]);
+			break;
+#endif
+#ifndef SELECTED
+		case '-':
+#endif
 		default:
 			printf("\
 usage:	 iplist-add [options]\n\
 options: -c <config file location>\n\
 	    default location at /etc/iplist/autoadd.conf\n");
+#ifdef SELECT_IPSET
+			printf("Compiled with ipset support\n");
+#endif
+#ifdef SELECT_NFT
+			printf("\
+	 -t <table name>\n\
+	    add to which table.\n\
+Compiled with nft support\n");
+#endif
+#ifndef SELECTED
+			printf("\
+	 --ipset\n\
+	    use ipset.\n\
+	 --nft\n\
+	    use nft.\n\
+	 -t <table name>\n\
+	    add to which table.\n");
+#endif
 			return EXIT_SUCCESS;
 		}
 
-	char	iplistFile[LENGTH_OF_NAME],
-		iplistName[LENGTH_OF_NAME],
-		iplistType[LENGTH_OF_TYPE],
-		iplistDbLocation[LENGTH_OF_LOCATION],
-		flushedSets[LENGTH_OF_LOCATION][LENGTH_OF_NAME],
-		*argvWrapper[LENGTH_OF_TYPE],
-		ipsetArgs[LENGTH_OF_TYPE][LENGTH_OF_NAME],
-		readBuffer[LENGTH_OF_NAME],
-		ipsetCommandBuf[LENGTH_OF_LOCATION];
-	memset(flushedSets, 0, LENGTH_OF_LOCATION * LENGTH_OF_NAME);
-	for (int pos = 0; pos < LENGTH_OF_TYPE; pos++)
-		argvWrapper[pos] = ipsetArgs[pos];
+	char	iplistFile[64],
+		iplistName[64],
+		iplistDbLocation[256],
+		letyoureadinwithhappy[256];
 
-	char confFileLocation[LENGTH_OF_LOCATION];
+	char confFileLocation[256];
 	sprintf(confFileLocation, "%s/%s", workpath, conffile);
-	FILE	*autoadd = fopen(confFileLocation, "r"),
-		*iplistDb = fopen(confFileLocation, "r");
-	if (autoadd != NULL) while (fscanf(autoadd, "%s %s %s", iplistFile, iplistName, iplistType) != EOF) {
-		sprintf(iplistDbLocation, "%s/db/%s", workpath, iplistFile);
-		freopen(iplistDbLocation, "r", iplistDb);
-		for (int pos = 0; pos < LENGTH_OF_LOCATION; pos++)
-			if (strcmp(flushedSets[pos], iplistName) == 0)
-				break;
-			else if (flushedSets[pos][0] == 0) {
-				strcpy(flushedSets[pos], iplistName);
-				memset(ipsetArgs, 0, LENGTH_OF_TYPE * LENGTH_OF_NAME);
-				strcpy(ipsetArgs[1], "-!");
-				strcpy(ipsetArgs[2], "create");
-				sprintf(ipsetArgs[3], "%s-4", iplistName);
-				sprintf(ipsetArgs[4], "hash:%s", iplistType);
-				strcpy(ipsetArgs[5], "family");
-				strcpy(ipsetArgs[6], "inet");
-				ipset_parse_argv(ipsetInterface, 7, argvWrapper);
-				sprintf(ipsetArgs[3], "%s-6", iplistName);
-				strcpy(ipsetArgs[6], "inet6");
-				ipset_parse_argv(ipsetInterface, 7, argvWrapper);
-				strcpy(ipsetArgs[2], "flush");
-				sprintf(ipsetArgs[3], "%s-4", iplistName);
-				ipset_parse_argv(ipsetInterface, 4, argvWrapper);
-				sprintf(ipsetArgs[3], "%s-6", iplistName);
-				ipset_parse_argv(ipsetInterface, 4, argvWrapper);
-				break;
-			}
+	FILE *autoadd = fopen(confFileLocation, "r");
 
-		printf("adding %s ... ", iplistFile);
-		int counter = 0;
-		while (fgets(readBuffer, LENGTH_OF_NAME, iplistDb) != NULL) {
-			char mode = 0;
-			for (int pos = 0; pos < LENGTH_OF_NAME; pos++)
-				switch (readBuffer[pos]) {
-				case '.':
-					mode |= 0x01;
-					break;
-				case ':':
-					mode |= 0x02;
-					break;
-				case 10:
-				case 13:
-					readBuffer[pos] = 0;
-					break;
-				}
-			switch (mode) {
-			case 0x01:
-				sprintf(ipsetCommandBuf, "add %s-4 %s", iplistName, readBuffer);
-				ipset_parse_line(ipsetInterface, ipsetCommandBuf); counter++;
-				break;
-			case 0x02:
-				sprintf(ipsetCommandBuf, "add %s-6 %s", iplistName, readBuffer);
-				ipset_parse_line(ipsetInterface, ipsetCommandBuf); counter++;
-				break;
-			default:
-				if (strcmp(readBuffer, ""))
-					printf("\n%s cannot be add", readBuffer);
-			}
-			memset(readBuffer, 0, LENGTH_OF_NAME);
-		}
-		printf("done, %d record(s) added.\n", counter);
+	if (autoadd != NULL) while (fscanf(autoadd, "%s %s %s", iplistFile, iplistName, letyoureadinwithhappy) != EOF) {
+		sprintf(iplistDbLocation, "%s/db/%s", workpath, iplistFile);
+		printf("adding %s to %s...", iplistFile, iplistName);
+		iplist_append(&iplist, fopen(iplistDbLocation, "r"), iplistName, nft_table_name);
 	} else printf("config file? where?\n");
-	fclose(iplistDb);
 	fclose(autoadd);
 
-	ipset_fini(ipsetInterface);
+#ifdef SELECT_IPSET
+	ipset_fini(operationInterface);
+#endif
+#ifdef SELECT_NFT
+	nft_ctx_free(operationInterface);
+#endif
 	return EXIT_SUCCESS;
 }
-
-/*
-char *argvWrapper(char argv[]) {
-}
-*/
